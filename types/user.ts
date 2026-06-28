e5bea9d244193376b9c658709aa1e211011cfade
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-28 19:12:53 +0530

    feat: add user subscription types (#31)
    * chore: exclude scripts from git clean
    
    * feat: add user subscription types

---
+++ types/user.ts
@@ -0,0 +1,3 @@
+﻿export type SubscriptionPlan = 'FREE'|'PRO'|'ENTERPRISE'
+export type SubscriptionStatus = 'ACTIVE'|'CANCELED'|'PAST_DUE'|'TRIALING'
+export interface UserSubscription { plan: SubscriptionPlan; status: SubscriptionStatus; cancelAtPeriodEnd: boolean }
