2d276cdd9db6af82a7dcee546ae4e49c7e2262f0
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-28 18:50:29 +0530

    feat: add object utilities

---
+++ lib/utils/object.ts
@@ -0,0 +1,3 @@
+﻿export const pick = <T extends object,K extends keyof T>(o: T, ks: K[]) => ks.reduce((a,k)=>({...a,[k]:o[k]}),{} as Pick<T,K>)
+export const omit = <T extends object,K extends keyof T>(o: T, ks: K[]) => { const r={...o}; ks.forEach(k=>delete r[k]); return r as Omit<T,K> }
+export const isEmpty = (o: object) => Object.keys(o).length===0
