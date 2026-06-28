2939ebc74c283717305b3ab1bfbf0dbbd3d5e14c
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-28 18:57:19 +0530

    test: add number toMs tests

---
+++ __tests__/utils/number2.test.ts
@@ -0,0 +1,6 @@
+﻿import { toMs } from '@/lib/utils/number'
+describe('toMs', () => {
+  it('secs', ()=>expect(toMs(1,'seconds')).toBe(1000))
+  it('mins', ()=>expect(toMs(1,'minutes')).toBe(60000))
+  it('hrs',  ()=>expect(toMs(1,'hours')).toBe(3600000))
+})
