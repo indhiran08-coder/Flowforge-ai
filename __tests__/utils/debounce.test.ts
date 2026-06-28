566ae4bc0626e0e4a4ad256e4a17070880aa1905
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-28 18:57:13 +0530

    test: add debounce tests

---
+++ __tests__/utils/debounce.test.ts
@@ -0,0 +1,11 @@
+﻿import { debounce } from '@/lib/utils/debounce'
+jest.useFakeTimers()
+describe('debounce', () => {
+  it('delays', ()=>{
+    const fn=jest.fn(); const d=debounce(fn,200)
+    d(); d(); d()
+    expect(fn).not.toHaveBeenCalled()
+    jest.advanceTimersByTime(200)
+    expect(fn).toHaveBeenCalledTimes(1)
+  })
+})
