381cdaa91f6be2720f805f6998226ad41ccbea43
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-28 19:06:44 +0530

    test: add validation tests (#100)
    * chore: exclude scripts from git clean
    
    * test: add validation tests

---
+++ __tests__/utils/validation.test.ts
@@ -0,0 +1,4 @@
+﻿import { isUrl, isEmail, isJson } from '@/lib/utils/validation'
+describe('isUrl',  () => { it('valid',()=>expect(isUrl('https://x.com')).toBe(true)); it('bad',()=>expect(isUrl('bad')).toBe(false)) })
+describe('isEmail',() => { it('valid',()=>expect(isEmail('a@b.com')).toBe(true)); it('bad',()=>expect(isEmail('bad')).toBe(false)) })
+describe('isJson', () => { it('valid',()=>expect(isJson('{}')).toBe(true)); it('bad',()=>expect(isJson('{bad')).toBe(false)) })
