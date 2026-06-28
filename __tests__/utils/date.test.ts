b9fc9f266fd6d42735165a7c9c80e50c73da6c93
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-28 18:56:01 +0530

    test: add date utility tests

---
+++ __tests__/utils/date.test.ts
@@ -0,0 +1,9 @@
+﻿import { fmtDur, isToday } from '@/lib/utils/date'
+describe('fmtDur', () => {
+  it('ms', () => expect(fmtDur(500)).toBe('500ms'))
+  it('s',  () => expect(fmtDur(2500)).toBe('2.5s'))
+  it('m',  () => expect(fmtDur(90000)).toBe('1.5m'))
+})
+describe('isToday', () => {
+  it('now', () => expect(isToday(new Date())).toBe(true))
+})
