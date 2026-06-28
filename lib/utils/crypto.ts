a995c6b7752c6f4d12f8248cca4e2d248a37ea57
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-28 18:51:11 +0530

    feat: add crypto utilities

---
+++ lib/utils/crypto.ts
@@ -0,0 +1,5 @@
+﻿import { createHash, createHmac, randomBytes } from 'crypto'
+export const sha256   = (d: string) => createHash('sha256').update(d).digest('hex')
+export const hmac256  = (d: string, s: string) => createHmac('sha256',s).update(d).digest('hex')
+export const token    = (n=32) => randomBytes(n).toString('hex')
+export const apiKey   = (p='ffa') => p+'_'+randomBytes(24).toString('base64url')
