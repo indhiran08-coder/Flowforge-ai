5cc7f1d4dae3bd9db03abd53c99b44786c7e3acb
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-28 18:56:07 +0530

    test: add string utility tests

---
+++ __tests__/utils/string.test.ts
@@ -0,0 +1,7 @@
+﻿import { truncate, capitalize, slugify } from '@/lib/utils/string'
+describe('truncate',   () => {
+  it('long', ()=>expect(truncate('Hello World',8)).toBe('Hello...'))
+  it('short',()=>expect(truncate('Hi',10)).toBe('Hi'))
+})
+describe('capitalize', () => { it('caps',()=>expect(capitalize('hello')).toBe('Hello')) })
+describe('slugify',    () => { it('slug',()=>expect(slugify('Hello World')).toBe('hello-world')) })
