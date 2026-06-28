a4cd02127285511c3499f175b143a04ad2dc1131
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-28 19:12:01 +0530

    feat: add retry utilities (#42)
    * chore: exclude scripts from git clean
    
    * feat: add retry utilities

---
+++ lib/utils/retry.ts
@@ -0,0 +1,7 @@
+﻿export const sleep = (ms: number) => new Promise<void>(r=>setTimeout(r,ms))
+export async function withRetry<T>(fn: ()=>Promise<T>, max=3, delay=1000): Promise<T> {
+  let last!: Error
+  for(let i=1;i<=max;i++) { try { return await fn() } catch(e) { last=e as Error; if(i<max) await sleep(delay*2**(i-1)) } }
+  throw last
+}
+export const withTimeout = <T>(p: Promise<T>, ms: number) => Promise.race([p, new Promise<never>((_,r)=>setTimeout(()=>r(new Error('Timeout '+ms+'ms')),ms))])
