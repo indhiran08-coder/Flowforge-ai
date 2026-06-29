d10f77a3a881c1ef414e15e68c903b44030ab9f9
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:31:38 +0530

    feat: add ShortcutHint component

---
+++ components/ui/extra/shortcut-hint.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// ShortcutHint component for FlowForge AI
+interface ShortcutHintProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function ShortcutHint(props: ShortcutHintProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default ShortcutHint
