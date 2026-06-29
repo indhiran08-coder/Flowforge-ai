e7a5ea9d0b269e43ee51a9b17edd101d7c6651fa
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:15:05 +0530

    feat: add WorkflowHeader component (#582)

---
+++ components/dashboard/workflow-header.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// WorkflowHeader dashboard component
+interface WorkflowHeaderProps {
+  className?: string
+}
+export function WorkflowHeader(props: WorkflowHeaderProps) {
+  return React.createElement('div', { className: props.className }, 'WorkflowHeader')
+}
