d168eff36eadb8922048931304755df1f5fbc171
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:15:49 +0530

    feat: add UsageChart component (#575)

---
+++ components/dashboard/usage-chart.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// UsageChart dashboard component
+interface UsageChartProps {
+  className?: string
+}
+export function UsageChart(props: UsageChartProps) {
+  return React.createElement('div', { className: props.className }, 'UsageChart')
+}
