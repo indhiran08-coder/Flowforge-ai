f333b7a5fbe3cfa03284a8504a89bb5c33439734
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:15:40 +0530

    feat: add stream service

---
+++ lib/services/stream.ts
@@ -0,0 +1,7 @@
+﻿// stream service for FlowForge AI
+export class streamService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const streamService = new streamService()
