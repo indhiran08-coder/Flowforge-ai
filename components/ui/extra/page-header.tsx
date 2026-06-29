1c3e0166a4fbc2e52c2615b800bb25d2d5f140c8
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:36:42 +0530

    feat: add PageHeader component (#363)

---
+++ components/ui/extra/page-header.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// PageHeader component for FlowForge AI
+interface PageHeaderProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function PageHeader(props: PageHeaderProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default PageHeader
