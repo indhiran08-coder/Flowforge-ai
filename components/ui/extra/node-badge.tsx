39d894a8a1521502b57c7f18beef901502c4e0b9
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:36:47 +0530

    feat: add NodeBadge component (#362)

---
+++ components/ui/extra/node-badge.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// NodeBadge component for FlowForge AI
+interface NodeBadgeProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function NodeBadge(props: NodeBadgeProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default NodeBadge
