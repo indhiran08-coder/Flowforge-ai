551c1b69c236883e01d42baa5b7967d299fa13cd
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 18:00:22 +0530

    feat: add CredentialCard component

---
+++ components/dashboard/credential-card.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// CredentialCard dashboard component
+interface CredentialCardProps {
+  className?: string
+}
+export function CredentialCard(props: CredentialCardProps) {
+  return React.createElement('div', { className: props.className }, 'CredentialCard')
+}
