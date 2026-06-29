b219451db82e81a95a34a6bfa3d07eb7dcf8f6cc
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:55:41 +0530

    feat: add execution service (#244)

---
+++ lib/services/execution.ts
@@ -0,0 +1,7 @@
+﻿// execution service for FlowForge AI
+export class executionService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const executionService = new executionService()
