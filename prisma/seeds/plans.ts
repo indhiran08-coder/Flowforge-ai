d64bed43e2478c5dc7d8c7678efa4871fddda939
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:45:03 +0530

    chore: add plans seed

---
+++ prisma/seeds/plans.ts
@@ -0,0 +1,8 @@
+﻿import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+// Seed: plans
+async function seed() {
+  console.log('Seeding plans...')
+  await prisma.$disconnect()
+}
+seed().catch(console.error)
