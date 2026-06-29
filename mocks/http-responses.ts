78cdadca90b152b87cb3382eed0c8795ff43c4e5
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:55:08 +0530

    chore: add http-responses mock

---
+++ mocks/http-responses.ts
@@ -0,0 +1,10 @@
+﻿// Mock factory for http-responses
+export const mockHttpResponses = {
+  create: (overrides = {}) => ({
+    id: 'mock-' + Math.random().toString(36).slice(2,8),
+    createdAt: new Date('2024-01-01'),
+    updatedAt: new Date('2024-01-01'),
+    ...overrides,
+  }),
+  list: (n = 3) => Array.from({ length: n }, (_, i) => mockHttpResponses.create({ id: 'mock-'+i })),
+}
