23f6f281553547b0324abe33016a5a36acbf26bc
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:25:38 +0530

    chore: add default-settings seed (#479)

---
+++ prisma/seeds/default-settings.ts
@@ -0,0 +1,8 @@
+﻿import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+// Seed: default-settings
+async function seed() {
+  console.log('Seeding default-settings...')
+  await prisma.$disconnect()
+}
+seed().catch(console.error)
