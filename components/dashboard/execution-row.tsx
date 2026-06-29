d5c96e9c67fa0269ac8341574353a23623b964a3
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:16:12 +0530

    feat: add ExecutionRow component (#571)

---
+++ components/dashboard/execution-row.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// ExecutionRow dashboard component
+interface ExecutionRowProps {
+  className?: string
+}
+export function ExecutionRow(props: ExecutionRowProps) {
+  return React.createElement('div', { className: props.className }, 'ExecutionRow')
+}
