5b78f3096bb481602c941d24f95ed98c0fa57bbf
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:15:11 +0530

    feat: add NodePalette component (#581)

---
+++ components/dashboard/node-palette.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// NodePalette dashboard component
+interface NodePaletteProps {
+  className?: string
+}
+export function NodePalette(props: NodePaletteProps) {
+  return React.createElement('div', { className: props.className }, 'NodePalette')
+}
