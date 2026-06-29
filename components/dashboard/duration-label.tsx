dc87a0433f77d8a6005fa1085826df1e656ef758
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:14:46 +0530

    feat: add DurationLabel component (#586)

---
+++ components/dashboard/duration-label.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// DurationLabel dashboard component
+interface DurationLabelProps {
+  className?: string
+}
+export function DurationLabel(props: DurationLabelProps) {
+  return React.createElement('div', { className: props.className }, 'DurationLabel')
+}
