8473f1f37834a88c694b46af446d52043e0770e5
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:55:31 +0530

    feat: add user service (#246)

---
+++ lib/services/user.ts
@@ -0,0 +1,7 @@
+﻿// user service for FlowForge AI
+export class userService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const userService = new userService()
