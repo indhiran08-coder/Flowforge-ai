6e530a47096c096389f127b9ebb52e93905e43c3
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:38:11 +0530

    feat: add DataTable component (#352)

---
+++ components/ui/extra/data-table.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// DataTable component for FlowForge AI
+interface DataTableProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function DataTable(props: DataTableProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default DataTable
