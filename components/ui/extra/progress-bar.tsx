9798db75167154c1b3ba1172c5c58d1d2f9871b4
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:31:17 +0530

    feat: add ProgressBar component

---
+++ components/ui/extra/progress-bar.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// ProgressBar component for FlowForge AI
+interface ProgressBarProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function ProgressBar(props: ProgressBarProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default ProgressBar
