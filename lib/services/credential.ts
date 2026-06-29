6fbbd80e2e7061768fb474e5f73e0b260e5eff31
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:55:36 +0530

    feat: add credential service (#245)

---
+++ lib/services/credential.ts
@@ -0,0 +1,7 @@
+﻿// credential service for FlowForge AI
+export class credentialService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const credentialService = new credentialService()
