90229a4d3159072b07914d9dd57ea4a208fcd6e2
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:40:04 +0530

    feat: add events constants (#330)

---
+++ lib/constants/extra/events.ts
@@ -0,0 +1,2 @@
+﻿// events constants for FlowForge AI
+export const EVENTS = { WORKFLOW_CREATED: 'workflow.created', EXECUTION_STARTED: 'execution.started', EXECUTION_DONE: 'execution.completed' } as const
