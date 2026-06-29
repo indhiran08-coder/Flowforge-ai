b1e022586c400927fd235a28b5887fdd1ed9c9dc
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:12:11 +0530

    feat: add scheduler service

---
+++ lib/services/scheduler.ts
@@ -0,0 +1,7 @@
+﻿// scheduler service for FlowForge AI
+export class schedulerService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const schedulerService = new schedulerService()
