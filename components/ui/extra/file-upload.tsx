199802cee57b6b3dec844847176bef91f5b4f150
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:29:45 +0530

    feat: add FileUpload component

---
+++ components/ui/extra/file-upload.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// FileUpload component for FlowForge AI
+interface FileUploadProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function FileUpload(props: FileUploadProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default FileUpload
