ccc1643ab3f1b093bf2bfadeb74ec19d5ee49be2
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:30:44 +0530

    feat: add LogViewer component

---
+++ components/ui/extra/log-viewer.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// LogViewer component for FlowForge AI
+interface LogViewerProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function LogViewer(props: LogViewerProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default LogViewer
