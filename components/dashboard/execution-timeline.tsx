ce268035e87f301340ced7eb13fa17f3f0e2889a
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:15:22 +0530

    feat: add ExecutionTimeline component (#579)

---
+++ components/dashboard/execution-timeline.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// ExecutionTimeline dashboard component
+interface ExecutionTimelineProps {
+  className?: string
+}
+export function ExecutionTimeline(props: ExecutionTimelineProps) {
+  return React.createElement('div', { className: props.className }, 'ExecutionTimeline')
+}
