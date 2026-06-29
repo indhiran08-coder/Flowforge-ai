6ba2e0784d892faa370cf9cb61bbe6611f04dae1
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:37:02 +0530

    feat: add InlineEdit component (#359)

---
+++ components/ui/extra/inline-edit.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// InlineEdit component for FlowForge AI
+interface InlineEditProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function InlineEdit(props: InlineEditProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default InlineEdit
