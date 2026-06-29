862494fe7b4c5ee4c4416c8f80b1cd2c8f54655a
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:29:26 +0530

    feat: add Divider component

---
+++ components/ui/extra/divider.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// Divider component for FlowForge AI
+interface DividerProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function Divider(props: DividerProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default Divider
