7d7ed00aa2640abe86139ebbfc3249e78ef593e4
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:26:48 +0530

    chore: add executions seed (#472)

---
+++ prisma/seeds/executions.ts
@@ -0,0 +1,8 @@
+﻿import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+// Seed: executions
+async function seed() {
+  console.log('Seeding executions...')
+  await prisma.$disconnect()
+}
+seed().catch(console.error)
