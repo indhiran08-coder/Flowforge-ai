acfcaa773c8f6278d1256c46d1cb286698d3cfe9
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:14:50 +0530

    feat: add TimeAgo component (#585)

---
+++ components/dashboard/time-ago.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// TimeAgo dashboard component
+interface TimeAgoProps {
+  className?: string
+}
+export function TimeAgo(props: TimeAgoProps) {
+  return React.createElement('div', { className: props.className }, 'TimeAgo')
+}
