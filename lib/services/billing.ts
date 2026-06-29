e3eb0e69fee8bfa926768e2c6fa7ac1982f2709d
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:53:12 +0530

    feat: add billing service (#261)

---
+++ lib/services/billing.ts
@@ -0,0 +1,7 @@
+﻿// billing service for FlowForge AI
+export class billingService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const billingService = new billingService()
