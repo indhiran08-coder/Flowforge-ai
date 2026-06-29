62072618aa43bf287a55c165faf11070a663e08d
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 18:01:50 +0530

    feat: add MobileWarning component

---
+++ components/dashboard/mobile-warning.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// MobileWarning dashboard component
+interface MobileWarningProps {
+  className?: string
+}
+export function MobileWarning(props: MobileWarningProps) {
+  return React.createElement('div', { className: props.className }, 'MobileWarning')
+}
