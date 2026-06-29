65647b3b5813a45c2560eaacf095fcf83b5d780a
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:27:08 +0530

    chore: add users seed (#468)

---
+++ prisma/seeds/users.ts
@@ -0,0 +1,8 @@
+﻿import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+// Seed: users
+async function seed() {
+  console.log('Seeding users...')
+  await prisma.$disconnect()
+}
+seed().catch(console.error)
