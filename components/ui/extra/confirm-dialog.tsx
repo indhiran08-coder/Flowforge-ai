4bba01496392c46ddf71321d2c4cf417f582dfcc
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:38:16 +0530

    feat: add ConfirmDialog component (#351)

---
+++ components/ui/extra/confirm-dialog.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// ConfirmDialog component for FlowForge AI
+interface ConfirmDialogProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function ConfirmDialog(props: ConfirmDialogProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default ConfirmDialog
