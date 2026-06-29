bafb98273701c8d833fd02bd5e0803548b202a9d
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:53:26 +0530

    feat: add queue service (#258)

---
+++ lib/services/queue.ts
@@ -0,0 +1,7 @@
+﻿// queue service for FlowForge AI
+export class queueService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const queueService = new queueService()
