00561a1bd2404707335ccb0923e9cc3afaecaac9
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:54:59 +0530

    feat: add template service (#252)

---
+++ lib/services/template.ts
@@ -0,0 +1,7 @@
+﻿// template service for FlowForge AI
+export class templateService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const templateService = new templateService()
