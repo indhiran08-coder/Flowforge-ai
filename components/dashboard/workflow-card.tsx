43e74ba7b3770bea9e0f4bcf8dd60bf4be78ff71
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:59:20 +0530

    feat: add WorkflowCard component

---
+++ components/dashboard/workflow-card.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// WorkflowCard dashboard component
+interface WorkflowCardProps {
+  className?: string
+}
+export function WorkflowCard(props: WorkflowCardProps) {
+  return React.createElement('div', { className: props.className }, 'WorkflowCard')
+}
