ee540954d17db3e0a2f94024f5a904d5417f0322
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:13:42 +0530

    feat: add storage service

---
+++ lib/services/storage.ts
@@ -0,0 +1,7 @@
+﻿// storage service for FlowForge AI
+export class storageService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const storageService = new storageService()
