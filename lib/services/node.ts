6480a1b6a208d929f4b01c5de1998086ecfa2ff4
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:53:02 +0530

    feat: add node service (#263)

---
+++ lib/services/node.ts
@@ -0,0 +1,7 @@
+﻿// node service for FlowForge AI
+export class nodeService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const nodeService = new nodeService()
