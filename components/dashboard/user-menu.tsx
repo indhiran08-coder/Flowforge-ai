729d8314f2f7cfe70571549d9af84e5c199cceff
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 18:01:33 +0530

    feat: add UserMenu component

---
+++ components/dashboard/user-menu.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// UserMenu dashboard component
+interface UserMenuProps {
+  className?: string
+}
+export function UserMenu(props: UserMenuProps) {
+  return React.createElement('div', { className: props.className }, 'UserMenu')
+}
