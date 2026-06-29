403da6005846d72dbabcefb9c10ad0f5d676d89e
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:38:51 +0530

    feat: add Breadcrumb component (#344)

---
+++ components/ui/extra/breadcrumb.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// Breadcrumb component for FlowForge AI
+interface BreadcrumbProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function Breadcrumb(props: BreadcrumbProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default Breadcrumb
