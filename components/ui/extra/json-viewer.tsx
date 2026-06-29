0ae37e3683047ddaae465f22908816db981f9eae
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:30:34 +0530

    feat: add JsonViewer component

---
+++ components/ui/extra/json-viewer.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// JsonViewer component for FlowForge AI
+interface JsonViewerProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function JsonViewer(props: JsonViewerProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default JsonViewer
