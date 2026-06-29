7dd6653779935a791215ddabc08fbcc8915fd07d
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:55:46 +0530

    feat: add workflow service (#243)

---
+++ lib/services/workflow.ts
@@ -0,0 +1,7 @@
+﻿// workflow service for FlowForge AI
+export class workflowService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const workflowService = new workflowService()
