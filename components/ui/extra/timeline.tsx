4d233e245ab3d19adb8414d4aaf7fe64b85b781e
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:32:07 +0530

    feat: add Timeline component

---
+++ components/ui/extra/timeline.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// Timeline component for FlowForge AI
+interface TimelineProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function Timeline(props: TimelineProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default Timeline
