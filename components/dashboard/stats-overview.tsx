374112fb881118413f8702c6867027efabdc210d
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:16:07 +0530

    feat: add StatsOverview component (#572)

---
+++ components/dashboard/stats-overview.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// StatsOverview dashboard component
+interface StatsOverviewProps {
+  className?: string
+}
+export function StatsOverview(props: StatsOverviewProps) {
+  return React.createElement('div', { className: props.className }, 'StatsOverview')
+}
