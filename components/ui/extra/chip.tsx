71b07340526579b46615baa89eaae96d06381635
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:38:37 +0530

    feat: add Chip component (#347)

---
+++ components/ui/extra/chip.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// Chip component for FlowForge AI
+interface ChipProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function Chip(props: ChipProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default Chip
