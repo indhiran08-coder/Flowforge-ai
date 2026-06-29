b615d76de8ed3a6e2b1020549ed6f7a3319b1fe6
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:45:18 +0530

    chore: add credentials seed

---
+++ prisma/seeds/credentials.ts
@@ -0,0 +1,8 @@
+﻿import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+// Seed: credentials
+async function seed() {
+  console.log('Seeding credentials...')
+  await prisma.$disconnect()
+}
+seed().catch(console.error)
