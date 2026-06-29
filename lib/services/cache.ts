ab4bf08ff6833a87084086eb6e35ad8182e36363
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:53:32 +0530

    feat: add cache service (#257)

---
+++ lib/services/cache.ts
@@ -0,0 +1,7 @@
+﻿// cache service for FlowForge AI
+export class cacheService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const cacheService = new cacheService()
