226c206c9022bbe4be6c5334255ad2efab651250
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:52:50 +0530

    feat: add logger service (#265)

---
+++ lib/services/logger.ts
@@ -0,0 +1,7 @@
+﻿// logger service for FlowForge AI
+export class loggerService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const loggerService = new loggerService()
