eabd186a099ae8a4e815cae0daae601522a889f0
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:15:16 +0530

    feat: add secret service

---
+++ lib/services/secret.ts
@@ -0,0 +1,7 @@
+﻿// secret service for FlowForge AI
+export class secretService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const secretService = new secretService()
