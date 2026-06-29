78b941b6bf95ef9ea5a2f52ff044292406a6527e
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:37:06 +0530

    feat: add InfoTooltip component (#358)

---
+++ components/ui/extra/info-tooltip.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// InfoTooltip component for FlowForge AI
+interface InfoTooltipProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function InfoTooltip(props: InfoTooltipProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default InfoTooltip
