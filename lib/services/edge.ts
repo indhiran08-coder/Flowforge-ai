0da6a433caebfabfbfcbdebac2bad5dbf529e371
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:52:56 +0530

    feat: add edge service (#264)

---
+++ lib/services/edge.ts
@@ -0,0 +1,7 @@
+﻿// edge service for FlowForge AI
+export class edgeService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const edgeService = new edgeService()
