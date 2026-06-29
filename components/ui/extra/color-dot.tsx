f827804dc0d706c3d9d50b29f00ff01c7b0ae113
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:29:00 +0530

    feat: add ColorDot component

---
+++ components/ui/extra/color-dot.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// ColorDot component for FlowForge AI
+interface ColorDotProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function ColorDot(props: ColorDotProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default ColorDot
