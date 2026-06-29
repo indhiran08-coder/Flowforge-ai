215635fef58de3642107fac704d52a61bbdd13e9
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:45:33 +0530

    chore: add webhook-templates seed

---
+++ prisma/seeds/webhook-templates.ts
@@ -0,0 +1,8 @@
+﻿import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+// Seed: webhook-templates
+async function seed() {
+  console.log('Seeding webhook-templates...')
+  await prisma.$disconnect()
+}
+seed().catch(console.error)
