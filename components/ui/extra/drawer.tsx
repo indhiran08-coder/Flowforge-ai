db42032589b717f5c6e2f4d024b94d932e17f182
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:29:33 +0530

    feat: add Drawer component

---
+++ components/ui/extra/drawer.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// Drawer component for FlowForge AI
+interface DrawerProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function Drawer(props: DrawerProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default Drawer
