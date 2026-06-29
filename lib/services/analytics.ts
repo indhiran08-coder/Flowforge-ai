e69b9ca9f75b18318a98d77a2c80e9e0285519ae
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:53:17 +0530

    feat: add analytics service (#260)

---
+++ lib/services/analytics.ts
@@ -0,0 +1,7 @@
+﻿// analytics service for FlowForge AI
+export class analyticsService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const analyticsService = new analyticsService()
