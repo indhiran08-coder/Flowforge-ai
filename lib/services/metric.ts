9584a01b1da3fe9b4bb657d07b310345e7aef646
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:14:54 +0530

    feat: add metric service

---
+++ lib/services/metric.ts
@@ -0,0 +1,7 @@
+﻿// metric service for FlowForge AI
+export class metricService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const metricService = new metricService()
