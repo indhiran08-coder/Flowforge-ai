054b777fe7e077eff87f89d6b47d1290f0f294b5
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:55:05 +0530

    feat: add http service (#251)

---
+++ lib/services/http.ts
@@ -0,0 +1,7 @@
+﻿// http service for FlowForge AI
+export class httpService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const httpService = new httpService()
