275f67b8b3e0f67aa867265bf43d26803db9ac00
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:15:34 +0530

    feat: add OnboardingSteps component (#577)

---
+++ components/dashboard/onboarding-steps.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// OnboardingSteps dashboard component
+interface OnboardingStepsProps {
+  className?: string
+}
+export function OnboardingSteps(props: OnboardingStepsProps) {
+  return React.createElement('div', { className: props.className }, 'OnboardingSteps')
+}
