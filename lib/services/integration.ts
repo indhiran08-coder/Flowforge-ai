e2165d4963e0f0d08424bec28b587e9a9957ce15
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:53:06 +0530

    feat: add integration service (#262)

---
+++ lib/services/integration.ts
@@ -0,0 +1,7 @@
+﻿// integration service for FlowForge AI
+export class integrationService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const integrationService = new integrationService()
