b77e90650f6be3430b617e0841e5285d29a4905a
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 18:01:02 +0530

    feat: add RunButton component

---
+++ components/dashboard/run-button.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// RunButton dashboard component
+interface RunButtonProps {
+  className?: string
+}
+export function RunButton(props: RunButtonProps) {
+  return React.createElement('div', { className: props.className }, 'RunButton')
+}
