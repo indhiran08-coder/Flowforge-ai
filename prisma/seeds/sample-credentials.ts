a74643fc8cbca7ef75f4a428e02460e296a54f5d
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:25:28 +0530

    chore: add sample-credentials seed (#481)

---
+++ prisma/seeds/sample-credentials.ts
@@ -0,0 +1,8 @@
+﻿import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+// Seed: sample-credentials
+async function seed() {
+  console.log('Seeding sample-credentials...')
+  await prisma.$disconnect()
+}
+seed().catch(console.error)
