3abd8fe10af3a9b0254b1b86ca32f6ff38e6873a
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:31:31 +0530

    feat: add Section component

---
+++ components/ui/extra/section.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// Section component for FlowForge AI
+interface SectionProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function Section(props: SectionProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default Section
