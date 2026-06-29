7a85404d776f2bde0fc200ec9894057a188f9d4f
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:55:16 +0530

    feat: add email service (#249)

---
+++ lib/services/email.ts
@@ -0,0 +1,7 @@
+﻿// email service for FlowForge AI
+export class emailService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const emailService = new emailService()
