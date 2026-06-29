0b26ddf6037df081c3aee26ee50a866836be0a53
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:45:10 +0530

    chore: add workflows seed

---
+++ prisma/seeds/workflows.ts
@@ -0,0 +1,8 @@
+﻿import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+// Seed: workflows
+async function seed() {
+  console.log('Seeding workflows...')
+  await prisma.$disconnect()
+}
+seed().catch(console.error)
