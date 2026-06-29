9a222b20cd0c78ce389f54174f6f9488c9399415
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:45:41 +0530

    chore: add node-templates seed

---
+++ prisma/seeds/node-templates.ts
@@ -0,0 +1,8 @@
+﻿import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+// Seed: node-templates
+async function seed() {
+  console.log('Seeding node-templates...')
+  await prisma.$disconnect()
+}
+seed().catch(console.error)
