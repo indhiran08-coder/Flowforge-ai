3488a65ee987e9a6ae3d239cb373ef08c589450f
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:31:09 +0530

    feat: add Pill component

---
+++ components/ui/extra/pill.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// Pill component for FlowForge AI
+interface PillProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function Pill(props: PillProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default Pill
