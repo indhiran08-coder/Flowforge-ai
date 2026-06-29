6491b78174fc7c9e4d1d5d4ea9af32fd6764864f
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:36:06 +0530

    feat: add TabGroup component (#370)

---
+++ components/ui/extra/tab-group.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// TabGroup component for FlowForge AI
+interface TabGroupProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function TabGroup(props: TabGroupProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default TabGroup
