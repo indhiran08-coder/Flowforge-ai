ad4f4b55284d550741e61c4410a973d9b72e69d1
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:35:56 +0530

    feat: add TruncatedText component (#372)

---
+++ components/ui/extra/truncated-text.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// TruncatedText component for FlowForge AI
+interface TruncatedTextProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function TruncatedText(props: TruncatedTextProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default TruncatedText
