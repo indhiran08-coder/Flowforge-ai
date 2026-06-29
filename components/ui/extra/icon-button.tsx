b6630df9dbe5f51471c9bea0492abc036c2fd00b
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:29:52 +0530

    feat: add IconButton component

---
+++ components/ui/extra/icon-button.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// IconButton component for FlowForge AI
+interface IconButtonProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function IconButton(props: IconButtonProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default IconButton
