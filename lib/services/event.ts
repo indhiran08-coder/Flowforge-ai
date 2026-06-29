08b610b24d4688ddd15e220ad5817a49e5510b9e
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:52:17 +0530

    feat: add event service (#271)

---
+++ lib/services/event.ts
@@ -0,0 +1,7 @@
+﻿// event service for FlowForge AI
+export class eventService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const eventService = new eventService()
