e4bd4b710fac2d62b91dd7e37aedf8605ad6f86d
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:11:26 +0530

    feat: add subscription service

---
+++ lib/services/subscription.ts
@@ -0,0 +1,7 @@
+﻿// subscription service for FlowForge AI
+export class subscriptionService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const subscriptionService = new subscriptionService()
