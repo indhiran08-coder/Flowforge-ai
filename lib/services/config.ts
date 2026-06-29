0749c417fca9ecea48f096fd4df8fb9d0337f733
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:52:33 +0530

    feat: add config service (#268)

---
+++ lib/services/config.ts
@@ -0,0 +1,7 @@
+﻿// config service for FlowForge AI
+export class configService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const configService = new configService()
