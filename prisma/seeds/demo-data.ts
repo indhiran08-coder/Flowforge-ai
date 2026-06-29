f17687d0535ffe54fa32d330288d9d75388f4a81
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:26:29 +0530

    chore: add demo-data seed (#476)

---
+++ prisma/seeds/demo-data.ts
@@ -0,0 +1,8 @@
+﻿import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+// Seed: demo-data
+async function seed() {
+  console.log('Seeding demo-data...')
+  await prisma.$disconnect()
+}
+seed().catch(console.error)
