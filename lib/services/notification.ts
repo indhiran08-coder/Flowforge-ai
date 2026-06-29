9f8eda4d04fec81bab7b5c37f7e8d1dc294955b4
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:54:48 +0530

    feat: add notification service (#254)

---
+++ lib/services/notification.ts
@@ -0,0 +1,7 @@
+﻿// notification service for FlowForge AI
+export class notificationService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const notificationService = new notificationService()
