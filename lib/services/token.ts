d0e284a65937907df2e9854bfd98efe67dc6674d
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:15:24 +0530

    feat: add token service

---
+++ lib/services/token.ts
@@ -0,0 +1,7 @@
+﻿// token service for FlowForge AI
+export class tokenService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const tokenService = new tokenService()
