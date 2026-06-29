b2def542c9f9d7a9af67e9aa1d1bd62cb1fbee03
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 18:00:38 +0530

    feat: add LogStream component

---
+++ components/dashboard/log-stream.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// LogStream dashboard component
+interface LogStreamProps {
+  className?: string
+}
+export function LogStream(props: LogStreamProps) {
+  return React.createElement('div', { className: props.className }, 'LogStream')
+}
