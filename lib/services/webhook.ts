0a9f3aa169b46a7d6bbf19f676480987caef7a27
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:55:21 +0530

    feat: add webhook service (#248)

---
+++ lib/services/webhook.ts
@@ -0,0 +1,7 @@
+﻿// webhook service for FlowForge AI
+export class webhookService {
+  async init() { return true }
+  async dispose() { return true }
+  isReady() { return true }
+}
+export const webhookService = new webhookService()
