89dea36b1aae21221559c6c7ec064f04ef3f1097
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:38:41 +0530

    feat: add StatCard component (#346)

---
+++ components/ui/extra/stat-card.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// StatCard component for FlowForge AI
+interface StatCardProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function StatCard(props: StatCardProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default StatCard
