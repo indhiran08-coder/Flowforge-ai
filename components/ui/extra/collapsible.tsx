ae1a8d984ac505545a85e3b6412dc75852243fe5
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:28:54 +0530

    feat: add Collapsible component

---
+++ components/ui/extra/collapsible.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// Collapsible component for FlowForge AI
+interface CollapsibleProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function Collapsible(props: CollapsibleProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default Collapsible
