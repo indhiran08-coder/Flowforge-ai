bf8c9a25051528fcf1f96b94ba58236b1b2ca7ff
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:37:21 +0530

    feat: add DurationBadge component (#355)

---
+++ components/ui/extra/duration-badge.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// DurationBadge component for FlowForge AI
+interface DurationBadgeProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function DurationBadge(props: DurationBadgeProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default DurationBadge
