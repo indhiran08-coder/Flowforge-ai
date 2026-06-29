d1f8f7de6d539cc8435c70fce06336df460f9f0d
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:46:54 +0530

    chore: add email-templates seed

---
+++ prisma/seeds/email-templates.ts
@@ -0,0 +1,8 @@
+﻿import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+// Seed: email-templates
+async function seed() {
+  console.log('Seeding email-templates...')
+  await prisma.$disconnect()
+}
+seed().catch(console.error)
