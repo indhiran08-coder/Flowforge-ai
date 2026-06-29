dac909a62901f7ad6a0710e46ce9fd8c8ebb45be
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:38:21 +0530

    feat: add CommandPalette component (#350)

---
+++ components/ui/extra/command-palette.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// CommandPalette component for FlowForge AI
+interface CommandPaletteProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function CommandPalette(props: CommandPaletteProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default CommandPalette
