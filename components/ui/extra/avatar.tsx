150f8aad9f894807aae5a192cf545b4fb9892ea2
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:38:57 +0530

    feat: add Avatar component (#343)

---
+++ components/ui/extra/avatar.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// Avatar component for FlowForge AI
+interface AvatarProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function Avatar(props: AvatarProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default Avatar
