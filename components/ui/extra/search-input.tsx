30b37fe21b523d0f25db2731c3559e47dfa61a5f
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:36:27 +0530

    feat: add SearchInput component (#366)

---
+++ components/ui/extra/search-input.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// SearchInput component for FlowForge AI
+interface SearchInputProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function SearchInput(props: SearchInputProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default SearchInput
