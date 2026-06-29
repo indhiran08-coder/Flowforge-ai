2daf00bfeb158610af36ae44a2af576b99e8e814
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:31:45 +0530

    feat: add StepIndicator component

---
+++ components/ui/extra/step-indicator.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// StepIndicator component for FlowForge AI
+interface StepIndicatorProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function StepIndicator(props: StepIndicatorProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default StepIndicator
