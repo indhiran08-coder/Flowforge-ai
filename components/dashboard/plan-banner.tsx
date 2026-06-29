67d15b67093a87b81deec79c4fd6f654d56246bb
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 18:00:07 +0530

    feat: add PlanBanner component

---
+++ components/dashboard/plan-banner.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// PlanBanner dashboard component
+interface PlanBannerProps {
+  className?: string
+}
+export function PlanBanner(props: PlanBannerProps) {
+  return React.createElement('div', { className: props.className }, 'PlanBanner')
+}
