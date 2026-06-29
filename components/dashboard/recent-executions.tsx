bbbd3874538003d822d83b9754d6943575e76056
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:16:02 +0530

    feat: add RecentExecutions component (#573)

---
+++ components/dashboard/recent-executions.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// RecentExecutions dashboard component
+interface RecentExecutionsProps {
+  className?: string
+}
+export function RecentExecutions(props: RecentExecutionsProps) {
+  return React.createElement('div', { className: props.className }, 'RecentExecutions')
+}
