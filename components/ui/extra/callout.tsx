2c61da1538844798fa4b5f503b1f0ba523bab60f
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:38:46 +0530

    feat: add Callout component (#345)

---
+++ components/ui/extra/callout.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+// Callout component for FlowForge AI
+interface CalloutProps {
+  className?: string
+  children?: React.ReactNode
+}
+export function Callout(props: CalloutProps) {
+  return React.createElement('div', { className: props.className }, props.children)
+}
+export default Callout
