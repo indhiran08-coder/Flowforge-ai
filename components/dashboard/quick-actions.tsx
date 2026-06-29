45acab8cc78d420b2c5bf60d1195941b3b85c23f
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 17:59:51 +0530

    feat: add QuickActions component

---
+++ components/dashboard/quick-actions.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// QuickActions dashboard component
+interface QuickActionsProps {
+  className?: string
+}
+export function QuickActions(props: QuickActionsProps) {
+  return React.createElement('div', { className: props.className }, 'QuickActions')
+}
