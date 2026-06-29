ed28f0257d12c408000f446e47d204d6f128fe69
INDHIRAN S <kingindhiran08@gmail.com>
2026-06-29 18:14:35 +0530

    feat: add PlanBadge component (#588)

---
+++ components/dashboard/plan-badge.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// PlanBadge dashboard component
+interface PlanBadgeProps {
+  className?: string
+}
+export function PlanBadge(props: PlanBadgeProps) {
+  return React.createElement('div', { className: props.className }, 'PlanBadge')
+}
