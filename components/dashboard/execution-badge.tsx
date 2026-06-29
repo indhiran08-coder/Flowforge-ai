d0ef1fcbf509e9223209c7e47be524ca70c25ffc
indhiran08-coder <kingindhiran08@gmail.com>
2026-06-29 18:01:11 +0530

    feat: add ExecutionBadge component

---
+++ components/dashboard/execution-badge.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react'
+// ExecutionBadge dashboard component
+interface ExecutionBadgeProps {
+  className?: string
+}
+export function ExecutionBadge(props: ExecutionBadgeProps) {
+  return React.createElement('div', { className: props.className }, 'ExecutionBadge')
+}
